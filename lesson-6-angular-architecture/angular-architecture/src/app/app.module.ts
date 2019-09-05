beb45a634947148dbd2cbc38dacca54e55b374c7
vishneuski <vishneuski87@gmail.com>
2019-09-05 21:43:43 +0400

    add userNameIput component

--- lesson-6-angular-architecture/angular-architecture/src/app/app.module.ts
+++ lesson-6-angular-architecture/angular-architecture/src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from './app.component';
 import { SuccessAlertComponent } from './success-alert/success-alert.component';
 import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { UserNameInputComponent } from './user-name-input/user-name-input.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SuccessAlertComponent,
-    WarningAlertComponent
+    WarningAlertComponent,
+    UserNameInputComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
