c7b0732a17d80d023cb0c850aa7e301da0051444
vishneuski <vishneuski87@gmail.com>
2019-09-15 22:11:22 +0400

    start validate the form

--- lesson-9-forms/forms/src/app/app.module.ts
+++ lesson-9-forms/forms/src/app/app.module.ts
@@ -1,9 +1,8 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from "@angular/forms";
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { UserFormComponent } from './user-form/user-form.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from "@angular/forms";
+import {AppComponent} from './app.component';
+import {UserFormComponent} from './user-form/user-form.component';
 
 @NgModule({
   declarations: [
@@ -12,7 +11,6 @@ import { UserFormComponent } from './user-form/user-form.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     FormsModule
   ],
   providers: [],
